60e274ac7f0f76da780b216e96fdadd875e1b913
optimisticlucifer <b20314@students.iitmandi.ac.in>
2023-01-11 17:50:42 +0530

    small bug fix in resume route

--- components/Header/index.js
+++ components/Header/index.js
@@ -76,8 +76,8 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
                   {showResume && (
                     <Button
-                      onClick={() =>
-                      }
+                      onClick={() => router.push("/resume")}
+                      classes="first:ml-1"
                     >
                       Resume
                     </Button>
